b53c7d13dd5476c8332b9f31a08b4fa880b9b021
Buddhi Prabodhani Kandambi <80638903+lkprabha@users.noreply.github.com>
2024-07-05 20:57:05 +1000

    add commnet

--- src/test/java/PageObjects/RegistrationPageObject.java
+++ src/test/java/PageObjects/RegistrationPageObject.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
+import java.time.Duration; i'm in adv-01'
 import  Utility.WebDriverHelper;
 
 public class RegistrationPageObject {
